cee77171c71a832401481590a35a98f4562b9c8d
cedrus <johnsangiolo@gmail.com>
2019-04-01 10:35:09 -0400

    initial commit

---
+++ src/main/java/com/cedrus/practice/todo/Spring_Boot_Todos/SpringBootTodosApplication.java
@@ -0,0 +1,13 @@
+package com.cedrus.practice.todo.Spring_Boot_Todos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTodosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cedrus/practice/todo/Spring_Boot_Todos/SpringBootTodosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cedrus.practice.todo.Spring_Boot_Todos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootTodosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
