61849af1adec50bc5e8cadb39f84332d6850d622
cedrus <johnsangiolo@gmail.com>
2019-04-02 09:27:33 -0400

    refactored ToDoItem class with Lombok

--- src/main/java/com/cedrus/practice/todo/Spring_Boot_Todos/model/ToDoItem.java
+++ src/main/java/com/cedrus/practice/todo/Spring_Boot_Todos/model/ToDoItem.java
@@ -1,11 +1,15 @@
 package com.cedrus.practice.todo.Spring_Boot_Todos.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter @Setter
 public class ToDoItem {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -25,14 +29,4 @@ public class ToDoItem {
         this(id, item);
         this.important = important;
     }
-
-    public int getId() { return this.id; }
-    public void setId(int id) { this.id = id;}
-
-    public String getItem() { return this.item; }
-    public void setItem(String item) { this.item = item; }
-
-    public boolean getImportant() { return this.important; }
-    public void setImportant(boolean important) { this.important = important; }
-
 }
